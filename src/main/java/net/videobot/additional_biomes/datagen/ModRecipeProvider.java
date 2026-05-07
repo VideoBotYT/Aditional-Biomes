73a58b910312275ad8d614372f20bfe65e080446
VideoBot <lander.meersman@gmail.com>
2026-05-07 15:05:53 +0200

    Datagen (finally works)

--- src/main/java/net/videobot/additional_biomes/datagen/ModRecipeProvider.java
+++ src/main/java/net/videobot/additional_biomes/datagen/ModRecipeProvider.java
@@ -2,15 +2,8 @@ package net.videobot.additional_biomes.datagen;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 
 import java.util.concurrent.CompletableFuture;
@@ -22,12 +15,6 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     @Override
     protected void buildRecipes(RecipeOutput recipeOutput) {
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Blocks.HOPPER)
-                .pattern("ILI")
-                .pattern("ILI")
-                .pattern(" I ")
-                .define('I', Items.IRON_INGOT)
-                .define('L', ItemTags.LOGS)
-                .unlockedBy("easy_hoper", has(Items.IRON_INGOT)).save(recipeOutput);
+
     }
 }
